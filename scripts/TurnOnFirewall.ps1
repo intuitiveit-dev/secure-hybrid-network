e72c070d27793c01419f6a8a310fdfe2c00ca737
intuitiveit-dev <dev@intuitiveit.ca>
2024-01-30 14:34:04 -0500

    add automation account template

---
+++ scripts/TurnOnFirewall.ps1
@@ -0,0 +1,6 @@
+connect-azaccount -identity
+$firewall = Get-AzFirewall -Name "AzureFirewall" -ResourceGroupName "NU-FWIIS-STAGING"
+$vnet = Get-AzVirtualNetwork -Name "vnet-hub" -ResourceGroupName "NU-FWIIS-STAGING"
+$publicip = Get-AzPublicIpAddress -Name "pip-firewall" -ResourceGroupName "NU-FWIIS-STAGING"
+$firewall.Allocate($vnet,$publicip)
+$firewall | Set-AzFirewall
